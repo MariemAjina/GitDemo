356ca172940ec586c6fde27348c7fec706a7b941
mariem <ajinamariem2@gmail.com>
2021-12-29 12:44:38 +0100

    first commit

---
+++ src/test/java/qaclickacamedy/Mavenjava/AppiumTest.java
@@ -0,0 +1,15 @@
+package qaclickacamedy.Mavenjava;
+
+import org.testng.annotations.Test;
+
+public class AppiumTest {
+	@Test
+	public void NativeAPpAndroid() {
+		System.out.println("NativeAPpAndroid");
+	}
+
+	@Test
+	public void IOSApps() {
+		System.out.println("IOSApps");
+	}
+}
